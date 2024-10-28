045627f9a4ef74e2df4121e7e1eca21605127865
nksound <NKSound@mail.ru>
2024-10-28 11:05:54 +0300

    "Простые Юнит-Тесты"

---
+++ test_12_1.py
@@ -0,0 +1,28 @@
+from runner import Runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner('TestWalk')
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('TestRun')
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner('Runner1')
+        runner2 = Runner('Runner2')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
